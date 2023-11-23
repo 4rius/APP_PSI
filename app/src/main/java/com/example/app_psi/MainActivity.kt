99e7a853962ca7ba03ba66610947d7d527c004f8
Santiago <santi.ari.pani@protonmail.com>
2023-11-23 22:44:20 +0100

    Layouts for the app

---
+++ app/src/main/java/com/example/app_psi/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.app_psi
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.ui.AppBarConfiguration
+import com.example.app_psi.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
