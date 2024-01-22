aa8240128eb9c6c328d8b6612ad82aefbecf4864
Santiago <santi.ari.pani@protonmail.com>
2024-01-22 20:42:02 +0100

    Update LogService.kt

--- app/src/main/java/com/example/app_psi/services/LogService.kt
+++ app/src/main/java/com/example/app_psi/services/LogService.kt
@@ -20,7 +20,6 @@ class LogService: Service() {
     lateinit var id: String
     lateinit var realtimeDatabase: FirebaseDatabase
     private val LOG_INTERVAL = 10000L
-    private val FB_RES_INTERVAL = 20000L
 
     override fun onCreate() {
         super.onCreate()
@@ -29,7 +28,6 @@ class LogService: Service() {
         node = NetworkService.getNode()!!
         id = node.id
         generalLog()
-        resLog()
     }
 
     private fun generalLog() {
@@ -51,52 +49,6 @@ class LogService: Service() {
         }
         handler.post(logRunnable)
     }
-
-    private fun resLog() {
-        val handler = Handler(Looper.getMainLooper())
-        val resRunnable: Runnable = object : Runnable {
-            @SuppressLint("SimpleDateFormat")
-            override fun run() {
-                val ramUsage: String = getRAMUsage()
-                //val cpuUsage: String = getCPUUsage()
-                val timestamp: String = SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(Date())
-                // Mandamos la info a la bd
-                sendResourceLog(id, timestamp, "Android","RAM", ramUsage)
-                //sendResourceLog(id, timestamp, "CPU", cpuUsage)
-                handler.postDelayed(this, FB_RES_INTERVAL)
-            }
-        }
-        handler.post(resRunnable)
-    }
-
-    private fun sendResourceLog(id: String, timestamp: String, t: String, s: String, ramUsage: String) {
-        // Lo subimos a la Firebase realtime database
-        val formattedId = id.replace(".", "-")
-        val ref = realtimeDatabase.getReference("logs/$formattedId/resources")
-        val version = packageManager.getPackageInfo(packageName, 0).versionName
-        val androidVersion = android.os.Build.VERSION.RELEASE
-        val formattedVersion = "$t $androidVersion"
-        val log = hashMapOf(
-            "id" to id,
-            "timestamp" to timestamp,
-            "version" to version,
-            "type" to formattedVersion,
-            "usage" to s,
-            "value" to ramUsage
-        )
-        ref.push().setValue(log)
-        Log.d(ContentValues.TAG, "Resources log sent to Firebase")
-    }
-
-    private fun getRAMUsage(): String {
-        val memInfo = ActivityManager.MemoryInfo()
-        val activityManager = getSystemService(ACTIVITY_SERVICE) as ActivityManager
-        activityManager.getMemoryInfo(memInfo)
-        val availableMem = memInfo.availMem / 0x100000L  // 0x100000L == 1048576L == 1024 * 1024 == 1MB
-        val totalMem = memInfo.totalMem / 0x100000L
-        val memUse = totalMem - availableMem
-        return "$memUse MB / $totalMem MB - ${memUse * 100 / totalMem}%"
-    }
     //private fun getCPUUsage(): String {} - De momento no se va a implementar por no poder leer el /proc/stat que era como se hacía pero se ha bloqueado por temas de seguridad.
 
 
@@ -115,10 +67,21 @@ class LogService: Service() {
                 "version" to version,
                 "type" to "Android " + android.os.Build.VERSION.RELEASE,
                 "activity_code" to acitvityCode,
-                "time" to time
+                "time" to time,
+                "RAM" to getRamUsage()
             )
             ref?.push()?.setValue(log)
             Log.d(ContentValues.TAG, "Activity log sent to Firebase")
+        }
+
+        private fun getRamUsage(): String {
+            val memInfo = ActivityManager.MemoryInfo()
+            val activityManager = instance?.getSystemService(ACTIVITY_SERVICE) as ActivityManager
+            activityManager.getMemoryInfo(memInfo)
+            val availableMem = memInfo.availMem / 0x100000L  // 0x100000L == 1048576L == 1024 * 1024 == 1MB
+            val totalMem = memInfo.totalMem / 0x100000L
+            val memUse = totalMem - availableMem
+            return "$memUse MB / $totalMem MB - ${memUse * 100 / totalMem}%"
         }
 
         var instance: LogService? = null
